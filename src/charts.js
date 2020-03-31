5bcce27e98bf4c5167f0454e48e23b0e38254a09
Matt Baker <mbaker@newrelic.com>
2020-03-30 22:51:37 -0700

    misc adjustments

--- src/charts.js
+++ src/charts.js
@@ -21,93 +21,11 @@ function renderInlineDelta(selector, data, attribute) {
     .text(numberFormatter)
 }
 
-function renderCaseChart(svg, reports, attribute) {
-  let chart = svg;
-  let width = 700;
-  let height = 400;
-  let day0 = reports[0];
-  let today = reports[reports.length - 1];
-  let margin = { left: 15, right: 45, bottom: 45, top: 15 };
-  let access = (report) => report[attribute];
-
-  let yScale = d3.scaleLinear()
-    .domain([0, today[attribute]])
-    .range([height - margin.bottom, margin.top])
-    .nice()
-
-  let xScale = d3.scaleTime()
-    .domain([day0.date, today.date])
-    .range([margin.left, width - margin.right])
-
-  let xAxis = d3.axisBottom(xScale)
-    .ticks(d3.timeDay.every(1))
-    .tickFormat(dateFormatter)
-
-  let yAxis = d3.axisRight(yScale);
-
-  let area = d3.area()
-    .curve(d3.curveLinear)
-    .x(report => xScale(report.date))
-    .y0(yScale(0))
-    .y1(report => yScale(access(report)))
-
-  let line = d3.line()
-    .defined(report => !isNaN(access(report)))
-    .x(report => xScale(report.date))
-    .y(report => yScale(access(report)))
-
-  chart.attr("viewBox", [0, 0, width, height])
-
-  chart.append("g")
-    .attr("transform", `translate(0, ${height - margin.bottom})`)
-    .call(xAxis)
-    .attr("font-family", "")
-    .attr("class", "numeral")
-    .selectAll("text")
-    .style("text-anchor", "end")
-    .attr("dx", "-.8em")
-    .attr("dy", ".15em")
-    .attr("transform", "rotate(-65)");
-
-
-  chart.append("g")
-    .attr("transform", `translate(${width - margin.right}, 0)`)
-    .call(yAxis)
-    .attr("font-family", "")
-    .attr("class", "numeral")
-
-  chart.append("path")
-    .datum(reports)
-    .attr("class", "case-area")
-    .attr("d", area);
-
-  chart.selectAll("rect")
-    .data(reports)
-    .enter()
-    .append("rect")
-    .attr("class", "case-bar")
-    .attr("x", report => xScale(report.date))
-    .attr("y", (report, i) => {
-      if (i === 0) return yScale(access(report))
-      return yScale(access(report) - access(reports[i - 1]))
-    })
-    .attr("height", (report, i) => {
-      if (i === 0) return yScale(0) - yScale(access(report))
-      return yScale(0) - yScale(access(report) - access(reports[i - 1]))
-    })
-    .attr("width", "1"); //xscale.bandwidth() ??
-
-  chart.append("path")
-    .datum(reports)
-    .attr("class", "case-line")
-    .attr("d", line)
-}
-
 function renderStackedCaseChart({chartSelector, data, attributes, colors}) {
   let svg = d3.select(chartSelector);
   let width = 700;
   let height = 400;
-  let margin = { left: 15, right: 45, bottom: 45, top: 15 };
+  let margin = { left: 20, right: 55, bottom: 55, top: 15 };
 
   let series = d3.stack().keys(attributes)(data)
   let xScale = d3.scaleTime()
@@ -125,9 +43,8 @@ function renderStackedCaseChart({chartSelector, data, attributes, colors}) {
 
     return g.attr("transform", `translate(0, ${height - margin.bottom})`)
       .call(axis)
-      .attr("font-family", "")
-      .attr("class", "numeral")
       .selectAll("text")
+      .attr("class", "axis-text")
       .style("text-anchor", "end")
       .attr("dx", "-.8em")
       .attr("dy", ".15em")
@@ -135,13 +52,13 @@ function renderStackedCaseChart({chartSelector, data, attributes, colors}) {
   }
 
   let yAxis = g => {
-    let axis = d3.axisRight(yScale);
+    let axis = d3.axisRight(yScale).ticks(8);
 
     return g
       .attr("transform", `translate(${width - margin.right}, 0)`)
       .call(axis)
       .selectAll("text")
-        .attr("class", "numeral")
+      .attr("class", "axis-text")
   }
 
   let area = d3.area()
